1f3029d8f1ef59d3c6d7ece4ed933442c48bb620
jadjer <jadjer@ya.ru>
2025-12-04 23:48:06 +0300

    Реализация контроллера шины IEBus

--- main/main.cpp
+++ main/main.cpp
@@ -4,8 +4,7 @@
 
 #include <esp_log.h>
 
-#include "ie_bus/Controller.hpp"
-#include "ie_bus/Driver.hpp"
+#include <iebus/Controller.hpp>
 
 auto constexpr TAG             = "CarLink";
 auto constexpr BUS_RX          = 8;
@@ -15,39 +14,8 @@ auto constexpr OUTPUT_ENABLE   = 6;
 // auto constexpr BUS_DEVICE_ADDR = 0x140;
 auto constexpr BUS_DEVICE_ADDR = 0x940;
 
-auto errorToString(MessageError const error) {
-    switch (error) {
-    case MessageError::BUS_READ_ERROR:
-        return "BUS_READ_ERROR";
-    case MessageError::BIT_SEQUENCE_ERROR:
-        return "BIT_SEQUENCE_ERROR";
-    case MessageError::BIT_PARITY_ERROR:
-        return "BIT_PARITY_ERROR";
-    case MessageError::BITS_COUNT_ERROR:
-        return "BITS_COUNT_ERROR";
-    case MessageError::START_BIT_READ_ERROR:
-        return "START_BIT_READ_ERROR";
-    case MessageError::BROADCAST_BIT_READ_ERROR:
-        return "BROADCAST_BIT_READ_ERROR";
-    case MessageError::MASTER_ADDRESS_READ_ERROR:
-        return "MASTER_ADDRESS_READ_ERROR";
-    case MessageError::SLAVE_ADDRESS_READ_ERROR:
-        return "SLAVE_ADDRESS_READ_ERROR";
-    case MessageError::CONTROL_DATA_READ_ERROR:
-        return "CONTROL_DATA_READ_ERROR";
-    case MessageError::DATA_LENGTH_READ_ERROR:
-        return "DATA_LENGTH_READ_ERROR";
-    case MessageError::DATA_READ_ERROR:
-        return "DATA_READ_ERROR";
-    case MessageError::ACKNOWLEDGE_ERROR:
-        return "ACKNOWLEDGE_ERROR";
-    default:
-        return "UNKNOWN_ERROR";
-    }
-}
-
 extern "C" [[noreturn]] void app_main() {
-    Controller controller(BUS_RX, BUS_TX, BUS_ENABLE, BUS_DEVICE_ADDR);
+    iebus::Controller controller(BUS_RX, BUS_TX, BUS_ENABLE, BUS_DEVICE_ADDR);
     controller.enable();
 
     while (true) {
@@ -57,7 +25,7 @@ extern "C" [[noreturn]] void app_main() {
 
             ESP_LOGI(TAG, "%01X %03X %03X %01X %d", message.broadcast, message.master, message.slave, message.control,
                 message.dataLength);
-            ESP_LOG_BUFFER_HEX(TAG, message.data, message.dataLength);
+            // ESP_LOG_BUFFER_HEX(TAG, message.data., message.dataLength);
             ESP_LOGI(TAG, "--------------");
         }
     }
