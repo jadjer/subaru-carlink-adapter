75b6131b43d0d9b5a6218ecdd6180b74c042eb81
jadjer <jadjer@ya.ru>
2025-12-25 12:55:37 +0300

    Code review

--- main/main.cpp
+++ main/main.cpp
@@ -18,8 +18,9 @@
 
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 
-#include <class/hid/hid.h>
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <iebus/Controller.hpp>
 
 #include "MessageParser.hpp"
@@ -37,41 +38,44 @@ auto constexpr IE_BUS_DEVICE_ADDR = 0x140;
 
 extern "C" [[noreturn]] void app_main() {
   USB usb;
-
   iebus::Controller mediaController(IE_BUS_RX, IE_BUS_TX, IE_BUS_ENABLE, IE_BUS_DEVICE_ADDR);
-  mediaController.enable();
 
   while (true) {
-    auto const optionalMessage = mediaController.readMessage();
-    if (optionalMessage.has_value()) {
-      auto const message = optionalMessage.value();
+    bool const isUsbConnected = usb.isConnected();
 
-      ESP_LOGI(TAG, "%s", message.toString().c_str());
+    if (isUsbConnected != mediaController.isEnabled()) {
+      isUsbConnected ? mediaController.enable() : mediaController.disable();
+    }
 
-      if (usb.isConnected()) {
-        auto const command = messageParse(message);
-        switch (command) {
-        case Command::PLAY:
-          usb.play();
-          break;
-        case Command::MUTE:
-          usb.mute();
-          break;
-        case Command::VOLUME_UP:
-          usb.volumeUp();
-          break;
-        case Command::VOLUME_DOWN:
-          usb.volumeDown();
-          break;
-        case Command::TRACK_NEXT:
-          usb.trackNext();
-          break;
-        case Command::TRACK_PREV:
-          usb.trackPrev();
-          break;
-        case Command::UNKNOWN:
-          break;
-        }
+    if (not isUsbConnected) {
+      vTaskDelay(pdMS_TO_TICKS(100));
+      continue;
+    }
+
+    if (auto const msg = mediaController.readMessage()) {
+      ESP_LOGI(TAG, "%s", msg->toString().c_str());
+
+      switch (messageParse(*msg)) {
+      case Command::PLAY:
+        usb.play();
+        break;
+      case Command::MUTE:
+        usb.mute();
+        break;
+      case Command::VOLUME_UP:
+        usb.volumeUp();
+        break;
+      case Command::VOLUME_DOWN:
+        usb.volumeDown();
+        break;
+      case Command::TRACK_NEXT:
+        usb.trackNext();
+        break;
+      case Command::TRACK_PREV:
+        usb.trackPrev();
+        break;
+      default:
+        break;
       }
     }
   }
