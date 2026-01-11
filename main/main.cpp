d646199c7beff00665293c7870af992047eaf864
jadjer <jadjer@ya.ru>
2026-01-11 03:50:41 +0300

    Добавлен класс для запуска обработчика в отдельном потоке

--- main/main.cpp
+++ main/main.cpp
@@ -19,7 +19,10 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 
 #include <esp_log.h>
-#include <iebus/ControllerThread.hpp>
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
+#include <iebus/Controller.hpp>
 
 namespace {
 
@@ -31,18 +34,59 @@ auto constexpr IE_BUS_DEVICE_ADDR = 0x140;
 
 } // namespace
 
-extern "C" void app_main() {
-  iebus::ControllerThread mediaController(IE_BUS_RX, IE_BUS_TX, IE_BUS_ENABLE, IE_BUS_DEVICE_ADDR);
+QueueHandle_t errorQueueHandle = nullptr;
+QueueHandle_t messageQueueHandle = nullptr;
 
+[[noreturn]] auto mediaWorker(void* pvParameters) -> void {
+  (void)pvParameters;
+
+  iebus::Controller mediaController(IE_BUS_RX, IE_BUS_TX, IE_BUS_ENABLE, IE_BUS_DEVICE_ADDR);
   mediaController.enable();
-  mediaController.start();
 
-  while (mediaController.isEnabled() and mediaController.isStarted()) {
-    auto const optionalMessage = mediaController.getMessage();
+  while (true) {
+    auto const expectedMessage = mediaController.readMessage();
 
-    if (optionalMessage.has_value()) {
-      auto const message = optionalMessage.value();
+    if (expectedMessage.has_value()) {
+      auto const message = expectedMessage.value();
+
+      xQueueSend(messageQueueHandle, &message, 1);
+    } else {
+      auto const error = expectedMessage.error();
+
+      xQueueSend(errorQueueHandle, &error, 1);
+    }
+  }
+}
+
+[[noreturn]] auto messageWorker(void* pvParameters) -> void {
+  (void)pvParameters;
+
+  while (true) {
+    iebus::Message message = {};
+
+    if (xQueueReceive(messageQueueHandle, &message, portMAX_DELAY) == pdTRUE) {
       ESP_LOGI(TAG, "%s", message.toString().c_str());
     }
   }
+}
+
+[[noreturn]] auto errorWorker(void* pvParameters) -> void {
+  (void)pvParameters;
+
+  while (true) {
+    iebus::MessageReadError error;
+
+    if (xQueueReceive(errorQueueHandle, &error, portMAX_DELAY) == pdTRUE) {
+      ESP_LOGE(TAG, "%u", error);
+    }
+  }
+}
+
+extern "C" void app_main() {
+  errorQueueHandle = xQueueCreate(100, sizeof(iebus::MessageReadError));
+  messageQueueHandle = xQueueCreate(100, sizeof(iebus::Message));
+
+  xTaskCreate(messageWorker, "message_worker", 4096, nullptr, 1, nullptr);
+  xTaskCreate(mediaWorker, "media_worker", 4096, nullptr, 5, nullptr);
+  xTaskCreate(errorWorker, "error_worker", 4096, nullptr, 5, nullptr);
 }
